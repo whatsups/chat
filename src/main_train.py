4906d1743a5ec1389d35cbd54f5a626fee7c6bc9
duye <duyeouc@126.com>
2019-04-09 09:08:10 +0800

    Update main_train.py

--- src/main_train.py
+++ src/main_train.py
@@ -7,18 +7,20 @@ if __name__ == '__main__':
     # 这里添加语料文件路径
     corpus = "corpus_path" 
     batch_size = 256
-    n_iteration = 100
+    n_iteration = 30000
     learning_rate = 0.0001
     n_layers = 4
     hidden_size = 512
     print_every = 5
     save_every = 20
+    bidir = True
     voc,pairs = loadPreparedData(corpus)
     train_batches = loadTraingingData(voc,pairs,corpus,batch_size,n_iteration)
     print("INFO:Start ...")
     dropout = 0.1
+    # 这里添加与训练模型路径，从头训练赋None
     load_pretrain = '1000_seq2seq_bidir_model.tar'
     # 训练
     train(load_pretrain,voc,train_batches,n_iteration,learning_rate,batch_size,n_layers,hidden_size,print_every,
-        save_every,dropout,use_ATTN=True,decoder_learning_ratio=5.0)
+        save_every,dropout,bidirectional=bidir,use_ATTN=True,decoder_learning_ratio=5.0)
     print("INFO:End ...")
